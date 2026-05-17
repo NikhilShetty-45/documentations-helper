6c169a4d92c871e80013985f7b93861425451c3c
Nikhil <nikhilwork45@gmail.com>
2026-05-17 22:35:56 +0530

    Completing all the import functions

---
+++ ingestion.py
@@ -0,0 +1,36 @@
+import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+
+import certifi
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter  # type: ignore[reportMissingImports]
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeSparseVectorStore
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
+
+from logger import (Colors, log_error, log_header, log_info, log_success, log_warning)
+
+load_dotenv(override=True)
+
+# Configure the SSL context to use certifi certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small", show_progress_bar=False, chunk_size=50, retry_min_seconds=10
+)
+
+#chroma = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
+vectorstore = PineconeSparseVectorStore(index_name="langchain-doc-helper-index", embedding=embeddings)
+tavily_extract = TavilyExtract()
+tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
+tavily_crawl = TavilyCrawl()
+
+
+
